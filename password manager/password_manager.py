e340da8499c1468585cf6a459e306c0df440b014
Aaditya Neupane <aaditya9803@gmail.com>
2022-08-22 00:23:59 +0545

    v.2.0 - Uses JSON

--- password manager/password_manager.py
+++ password manager/password_manager.py
@@ -1,27 +1,61 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import json
 
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50,pady=50)
 
+def search():
+    website = website_entry.get()
+    try:
+        with open("password manager/data.json","r") as data_file:
+            data=json.load(data_file)
+    except:
+        messagebox.showinfo(title="login details", message="No login details of {}".format(website))
+    else:
+        if website in data:
+                email = data[website]["email"]
+                password = data[website]["password"]
+                messagebox.showinfo(title="login details", message="email = {} \n password = {}".format(email,password))
+        else:
+            messagebox.showinfo(title="File not found", message="No information has been saved")
+        
 
 def save():
     website = website_entry.get()
     email = id_entry.get()
     password = password_entry.get()
+    new_dict = {
+        website:{
+            "email": email,
+            "password": password
+        }
+    }
     if len(email) == 0 or len(password)==0:
         messagebox.askokcancel(title="Incorrect info", message="Email/Password cannot be empty")
     else:
         istrue = messagebox.askokcancel(title="check info", message="Are your information correct? \n Email: {}\n Password:{}".format(email,password))
         if istrue:
-            with open("password manager/data.txt","a") as data_file:
-                data_file.write("{} | {} | {}\n".format(website,email,password))
-    password_entry.delete(0,END)
-    id_entry.delete(0,END)
-    website_entry.delete(0,END)
-    website_entry.focus()
+            try:
+                with open("password manager/data.json","r") as data_file:
+                    data = json.load(data_file)
+                    
+            except:
+                with open("password manager/data.json","w") as data_file:
+                    json.dump(new_dict,data_file,indent=4)
+            
+            else:                                  
+                data.update(new_dict)
+                with open("password manager/data.json","w") as data_file:
+                    json.dump(data,data_file,indent=4)
+                    
+            finally:
+                password_entry.delete(0,END)
+                id_entry.delete(0,END)
+                website_entry.delete(0,END)
+                website_entry.focus()
     
     
 def fill_password():
@@ -83,6 +117,9 @@ generate_password = Button(text="Generate Password",command=fill_password)
 generate_password.grid(row=4, column=1)
 add_button = Button(text="Add",command=save)
 add_button.grid(row=4,column=1, columnspan=2, sticky=E)
+search_button = Button(text="Search",command=search)
+search_button.grid(row=4,column=0)
+
 
 
 window.mainloop()
