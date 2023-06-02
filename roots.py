5abf573f4a3c7d3c4078670c72d568279a7b755a
Aaditya Neupane <aaditya9803@gmail.com>
2023-06-03 05:17:40 +0545

    OKAY

--- roots.py
+++ roots.py
@@ -1,20 +1,31 @@
-func = input("enter function - ")
-# ax^2+bx+c
-# a = func.split('+')
-forsquare = []
-print (func)
-for _ in func:
-    forsquare = forsquare + [*_]
-forsquare = forsquare + [""]
-print (forsquare)
-for i in range(len(forsquare)):
-    if (forsquare[i].isalpha() == True) and (forsquare[i] == "^"):
+func = input("enter function - ") # ax^2+bx+c
+func = [*func] + ['']
+
+def splitter(the_func):
+    splitted = the_func.split('+')
+    return splitter
+    
+def replacer(the_func): #Removes Spaces
+    clear_func =[]
+    i = 0
+    # if the_func[i] == 'x':
+    #     the_func_1 = ['1'] + [the_func]
+         
+    for i in range(len(the_func)):
+        if the_func[i] != chr(32):
+            clear_func = clear_func + [the_func[i]]
         
-    if forsquare[i] == "^":
-        forsquare[i-2] = int(forsquare[i-2]) * int(forsquare[i+1])
-        forsquare[i+1] = int(forsquare[i+1])-1        
-        forsquare[i] = "**"
-    # if (forsquare[i].isalpha()) and (forsquare[i+1].isalpha()) == True:
-    #     print("hello")
-            
-print (forsquare)
+    return clear_func
+
+def derivative(the_func): 
+    i = 0
+    for i in range(len(the_func)):
+        if the_func[i] == "^":
+            the_func[i-2] = int(the_func[i-2]) * int(the_func[i+1])
+            the_func[i+1] = int(the_func[i+1])-1
+    return the_func
+        
+
+
+print(derivative(replacer(func)))
+
